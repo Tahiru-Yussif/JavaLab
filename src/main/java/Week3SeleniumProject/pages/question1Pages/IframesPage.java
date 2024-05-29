b638a9b60b7f64a54ee7a34b4e2c1cf9b6dcb70f
TahiruYussif <yussiftahiru555@gmail.com>
2024-05-29 00:04:49 +0000

    update of the iframe

--- src/main/java/Week3SeleniumProject/pages/question1Pages/IframesPage.java
+++ src/main/java/Week3SeleniumProject/pages/question1Pages/IframesPage.java
@@ -1,46 +1,52 @@
-package Week3SeleniumProject.pages.question1Pages;
+    package Week3SeleniumProject.pages.question1Pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+    import org.openqa.selenium.By;
+    import org.openqa.selenium.JavascriptExecutor;
+    import org.openqa.selenium.WebDriver;
+    import org.openqa.selenium.WebElement;
+    import org.openqa.selenium.support.ui.ExpectedConditions;
+    import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+    import java.time.Duration;
 
-public class IframesPage {
-    private final WebDriver driver;
-    private final By iframesButton = By.linkText("Iframes");
+    public class IframesPage {
+        private final WebDriver driver;
+        private final By iframesButton = By.linkText("Iframes");
+        private final By firstIframeSubscribeButton = By.xpath("//span[text()='Subscribe']");
+        private final By secondIframeDownloadButton = By.xpath("//span[text()='Downloads']");
 
-    public IframesPage(WebDriver driver) {
-        this.driver = driver;
+        public IframesPage(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        public void clickIframesButton() {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement buttonIframe = wait.until(ExpectedConditions.visibilityOfElementLocated(iframesButton));
+
+            // Scroll into view
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", buttonIframe);
+
+            // Wait for the element to be clickable
+            wait.until(ExpectedConditions.elementToBeClickable(buttonIframe));
+
+            // Use JavaScript to click the button
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", buttonIframe);
+        }
+
+        private boolean checkIframe(int iframeIndex, By elementLocator) {
+            driver.switchTo().frame(iframeIndex);
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(elementLocator));
+            boolean isDisplayed = element.isDisplayed();
+            driver.switchTo().defaultContent();
+            return isDisplayed;
+        }
+
+        public boolean isFirstIframeSubscribeButtonDisplayed() {
+            return checkIframe(0, firstIframeSubscribeButton);
+        }
+
+        public boolean isSecondIframeDownloadButtonDisplayed() {
+            return checkIframe(1, secondIframeDownloadButton);
+        }
     }
-
-    public void clickIframesButton() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement buttonIframe = wait.until(ExpectedConditions.visibilityOfElementLocated(iframesButton));
-
-        // Scroll into view
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", buttonIframe);
-
-        // Wait for the element to be clickable
-        wait.until(ExpectedConditions.elementToBeClickable(buttonIframe));
-
-        // Use JavaScript to click the button
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", buttonIframe);
-    }
-
-    public void switchToFirstIframe() {
-        driver.switchTo().frame(0);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='OK']")));
-        loginButton.click();
-    }
-
-    public void loginInFirstIframe() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement subscribeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Subscribe']")));
-        System.out.println(subscribeButton.getText());
-    }
-}
--- src/test/java/tests/question1/Iframes.java
+++ src/test/java/tests/question1/Iframes.java
@@ -24,20 +24,28 @@ public class Iframes extends BaseTest {
     }
 
     @Test
-    public void testFirstIframe() throws InterruptedException {
+    public void testFirstIframe() {
         // calling the testClickIframeButton method
         testClickIframeButton();
 
         // Instantiate the IframePage
-        IframesPage iframe = new IframesPage(driver);
+        IframesPage iframes = new IframesPage(driver);
 
-        Thread.sleep(5000);
+        // verify that the subscribe button display on the first iframe
+        boolean isDisplayed = iframes.isFirstIframeSubscribeButtonDisplayed();
+        Assert.assertTrue(isDisplayed, "The text name isn't found");
+    }
 
-        // perform test on the first Iframe
-        iframe.switchToFirstIframe();
-        iframe.loginInFirstIframe();
+    @Test
+    public void testSecondIframe() {
+        // calling the testClickIframeButton method
+        testClickIframeButton();
 
-        Thread.sleep(5000);
-//        Assert.assertTrue(true, "String message");
+        // Instantiate the IframePage
+        IframesPage iframes = new IframesPage(driver);
+
+        // verify that the subscribe button display on the first iframe
+        boolean isDisplayed = iframes.isSecondIframeDownloadButtonDisplayed();
+        Assert.assertTrue(isDisplayed, "The text name isn't found");
     }
 }
